cd91026c47f6fa082d63b3677a6952cbadf542b6
John Harvey <johnharvey.usa@gmail.com>
2026-04-27 15:39:27 -0600

    fix tile URL

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['leaflet', 'react-leaflet'],
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
